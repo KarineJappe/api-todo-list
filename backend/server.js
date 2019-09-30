5227d7a30fe10145310c83ba6ff8c6e1adbc978a
KarineJappe <karine_jappe@hotmail.com>
2019-09-30 12:58:42 -0300

    Adcionando pastas

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+require('./routes.js')(server);
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb+srv://trabalhog2:trabalhog2@cluster0-aozv0.mongodb.net/trabalhog2?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.get('/', (req, res) => {
+    res.json({"message": "Orientação a objetos 1"});
+});
+server.listen(3000)
+
+
